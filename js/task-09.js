a7e40d75939aee1a402c191be173bbe9cca2d8b6
EehorV <vasylchenkoyhor@gmail.com>
2023-06-05 23:15:22 +0300

    9 tsk

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btnChangeColor = document.querySelector(".change-color");
+const colorValue = document.querySelector(".color");
+const bodyElem = document.body;
+
+btnChangeColor.addEventListener("click", () => {
+  const bodyElementColor = (bodyElem.style.backgroundColor =
+    getRandomHexColor());
+  colorValue.textContent = bodyElementColor;
+});
