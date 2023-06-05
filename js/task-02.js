26ed4aabe9ffddf0d58f8e7e11e1524b8dcd9dc6
EehorV <vasylchenkoyhor@gmail.com>
2023-06-05 17:47:57 +0300

    2 tsk

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,15 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listOfItems = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  return item;
+});
